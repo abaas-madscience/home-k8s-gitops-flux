f6f008cf7f9b6eb90faf5cf023fce7b8efbcc848
Oscar <oscarfrowijn@gmail.com>
2025-04-22 18:23:52 +0200

    commit latest

--- test/install-cilium.sh
+++ test/install-cilium.sh
@@ -1,3 +1,5 @@
+!!! -> WRONG !kubectl apply -k "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v1.0.0"
+
 helm upgrade --install cilium cilium/cilium \
   --version 1.17.3 \
   --namespace kube-system \
@@ -11,4 +13,17 @@ helm upgrade --install cilium cilium/cilium \
   --set ciliumIngress.enabled=true \
   --set ciliumIngress.replicas=1 \
   --set operator.enabled=true \
-  --set operator.manageCRDs=true
+  --set operator.manageCRDs=true \
+  --set gatewayAPI.enabled=true \
+  --set gatewayAPI.secretsNamespace.name=kube-system \
+  --set gatewayAPI.secretsNamespace.create=false \
+  --set gatewayAPI.secretsNamespace.sync=true
+
+kubectl get gatewayclass -o yaml
+
+kubectl -n kube-system rollout restart deploy/cilium-operator
+
+
+READ THE DOCS !!!
+
+kubectl get gatewayclass
