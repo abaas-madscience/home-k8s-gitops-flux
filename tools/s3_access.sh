0cd4e6fa156bbab0352bbdcdf2efe3b7714bf1a0
Oscar <oscarfrowijn@gmail.com>
2025-09-05 10:15:27 +0200

    Removed minio PVC

---
+++ tools/s3_access.sh
@@ -0,0 +1,3 @@
+echo "AccessKey: $(kubectl get secret rook-ceph-object-user-knowledgebase-ai-pipeline-user -n rook-ceph -o jsonpath='{.data.AccessKey}' | base64 -d)"
+❯ echo "SecretKey: $(kubectl get secret rook-ceph-object-user-knowledgebase-ai-pipeline-user -n rook-ceph -o jsonpath='{.data.SecretKey}' | base64 -d)"
+
