fd4c2807f47413d4cc1340bdfea88ee15c825c02
Oscar <oscarfrowijn@gmail.com>
2025-09-10 13:19:56 +0200

    Pipeline integration

--- tools/s3_access.sh
+++ tools/s3_access.sh
@@ -1,3 +1,4 @@
 echo "AccessKey: $(kubectl get secret rook-ceph-object-user-knowledgebase-ai-pipeline-user -n rook-ceph -o jsonpath='{.data.AccessKey}' | base64 -d)"
-❯ echo "SecretKey: $(kubectl get secret rook-ceph-object-user-knowledgebase-ai-pipeline-user -n rook-ceph -o jsonpath='{.data.SecretKey}' | base64 -d)"
+echo "SecretKey: $(kubectl get secret rook-ceph-object-user-knowledgebase-ai-pipeline-user -n rook-ceph -o jsonpath='{.data.SecretKey}' | base64 -d)"
+echo "use mcli alias set ceph ACCESSKEY SECRET"
 
